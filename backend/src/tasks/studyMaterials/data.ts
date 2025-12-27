05ed676959e99c62bc9fd7a5e97cf864c146b3f3
VladimirLQA <vovaomgxdlazurenko@gmail.com>
2025-12-27 23:52:20 +0200

    update data

--- backend/src/tasks/studyMaterials/data.ts
+++ backend/src/tasks/studyMaterials/data.ts
@@ -1,8 +1,28 @@
-export const studyMaterials = {
+export const studyMaterials: Record<'locatorMethods', Record<string, { title: string, url: string }>> = {
     locatorMethods: {
         getByText: {
             title: "Locator method - getByText()",
             url: "https://playwright.dev/docs/api/class-locator#locator-get-by-text",
+        },
+        getByRole: {
+            title: "Locator method - getByRole()",
+            url: "https://playwright.dev/docs/api/class-locator#locator-get-by-role",
+        },
+        getByPlaceholder: {
+            title: "Locator method - getByPlaceholder()",
+            url: "https://playwright.dev/docs/api/class-locator#locator-get-by-placeholder",
+        },
+        getByAltText: {
+            title: "Locator method - getByAltText()",
+            url: "https://playwright.dev/docs/api/class-locator#locator-get-by-alt-text",
+        },
+        getByTestId: {
+            title: "Locator method - getByTestId()",
+            url: "https://playwright.dev/docs/api/class-locator#locator-get-by-test-id",
+        },
+        getByTitle: {
+            title: "Locator method - getByTitle()",
+            url: "https://playwright.dev/docs/api/class-locator#locator-get-by-title",
         },
     },
 };
