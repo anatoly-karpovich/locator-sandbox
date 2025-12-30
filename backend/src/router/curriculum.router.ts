493a19e5df9d6a7c084bab8468c42d32cff97718
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-12-31 00:23:40 +0100

    removed curriculum

--- backend/src/router/curriculum.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { CurriculumController } from "../controllers/curriculum.controller";
-
-const curriculumRouter = Router();
-const curriculumController = new CurriculumController();
-
-curriculumRouter.get("/curriculum", curriculumController.get);
-
-export default curriculumRouter;
